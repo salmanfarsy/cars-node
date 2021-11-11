3502877a60b444b9090b307480cd39b59ebfec47
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-11-11 23:41:56 +0600

    initialing again

---
+++ app.js
@@ -0,0 +1,32 @@
+//modules
+const express = require('express'),
+app = express()
+dotenv = require('dotenv'),
+cors = require('cors'),
+carsRoute = require('./routes/carsRoute'),
+orderRoute = require('./routes/orderRoute'),
+userRoute = require('./routes/userRoute'),
+reviewRoute = require('./routes/reviewRoute'),
+mongoose = require('mongoose');
+//config
+dotenv.config()
+app.use(express.json());
+app.use(cors());
+//mongoose
+mongoose.connect(process.env.MONGO_URL).then(()=> console.log('database connected')).catch((error)=> console.log('not Connected'))
+
+
+
+//routes
+app.get('/', (req, res)=>{
+    res.json('Hello this Server is Running')
+})
+app.use('/api', carsRoute);
+app.use('/api', orderRoute);
+app.use('/api', userRoute);
+app.use('/api', reviewRoute);
+//port
+
+app.listen(5000, ()=>{
+    console.log('your server is on')
+})
